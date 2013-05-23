6ce349735cbc6478573e6ed8b882c4383679dcf3
guesslara <glara.styleweb@gmail.com>
2013-05-23 12:26:00 -0500

    Primer Envio

---
+++ includes/txtApp.php
@@ -0,0 +1,38 @@
+<?php
+	$txtApp = array();
+	//variables para la sesion
+	$txtApp['session']['name'] = "controlBounce";
+	$txtApp['session']['register'] = "controlBounce";
+	$txtApp['session']['nivelUsuario'] = "nivelUsuarioBounce";
+	$txtApp['session']['loginUsuario'] = "loginUsuarioBounce";
+	$txtApp['session']['passwordUsuario'] = "passwordUsuarioBounce";
+	$txtApp['session']['idUsuario'] = "idUsuarioBounce";
+	$txtApp['session']['nombreUsuario'] = "nombreUsuarioBounce";
+	$txtApp['session']['apellidoUsuario'] = "apellidoUsuarioBounce";
+	$txtApp['session']['origenSistemaUsuario'] = "origenSistemaUsuarioBounce";
+	$txtApp['session']['origenSistemaUsuarioNombre'] = "origenControlEsqueletoBounce";
+	$txtApp['session']['cambiarPassUsuario'] = "cambiarPassUsuarioBounce";
+	$txtApp['session']['sexoUsuario'] = "sexoUsuarioBounce";
+	$txtApp['session']['nominaUsuario'] = "nominaUsuarioBounce";
+	$txtApp['session']['idProyectoSeleccionado'] = "idProyectoSeleccionado";
+	//login de usuarios
+	$txtApp['login']['tituloAppTitle'] = "Nextel Refurbish";
+	$txtApp['login']['tituloAppPrincipal'] = "IQe. Sisco - Bounce Refurbish";
+	$txtApp['login']['tituloUsuario'] = "Usuario:";
+	$txtApp['login']['tituloPass'] = "Password:";
+	$txtApp['login']['btnApp'] = "Entrar";
+	$txtApp['login']['tituloAbout'] = "Acerca de...";
+	$txtApp['login']['pieLogin'] = "Acceso Intranet IQelectronics International ".date("Y");
+	//App GUI Principal
+	$txtApp['appPrincipal']['tituloPrincipalApp'] = "Bounce Refurbish";
+	$txtApp['appPrincipal']['cerrarSesion'] = "Cerrar Sesi&oacute;n";
+	$txtApp['appPrincipal']['msgModulo'] = "Bounce Refurbish";
+	$txtApp['appPrincipal']['msgActualizaciones'] = "Actualizaciones";
+	$txtApp['appPrincipal']['msgReportarError'] = "Reportar Problema";
+	$txtApp['appPrincipal']['msgBarraCarga'] = "Listo";
+	//Mensajes de error
+	$txtApp['mensajeError']['salirApp'] = "Si decide continuar, se pueden perder los cambios que no alla Guardado.";
+	//Pagina Profile
+	$txtApp['appProfile']['tituloPagina'] = "Informaci&oacute;n del Perfil";
+	$txtApp['appProfile']['tituloModulo'] = "Perfil de Usuario";	
+?>
